6d83a3b07f051e95abe185737119587c40ab3ce5
Aleksey Malevaniy <a.malevaniy@gmail.com>
2014-12-01 17:49:05 +0200

    Gradle plugin updated to RC1

--- src/main/java/com/example/DeckardApplication.java
+++ src/main/java/com/example/DeckardApplication.java
@@ -1,7 +1,22 @@
 package com.example;
 
-import android.app.Application;
+import android.content.Context;
+import android.support.multidex.MultiDexApplication;
 
-public class DeckardApplication extends Application {
+public class DeckardApplication extends MultiDexApplication {
 
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        attachBaseContext(this);
+    }
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        try {
+            super.attachBaseContext(base);
+        } catch (RuntimeException ignored) {
+            // Multidex support doesn't play well with Robolectric yet
+        }
+    }
 }
